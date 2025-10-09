1b90c4fac169a6daa47c8786be2469162313ca19
Geraltwolf-bit <geraltwolf@yandex.ru>
2025-10-09 07:56:53 +0000

    fixed app

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,7 +1,13 @@
 import streamlit as st
-st.header('st.button')
-clicked = st.button("Click me!")
-if clicked:
-    st.write('Hellow there!')
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def toggle_message():
+    st.session_state.clicked = not st.session_state.clicked
+
+st.button("Click me!", on_click=toggle_message)
+
+if st.session_state.clicked:
+    st.write("Hello!")
 else:
-    st.write('Goodbye')
+    st.write("Buy!")
