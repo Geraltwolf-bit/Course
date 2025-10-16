fa877d566e05659ae633f3b705965c9cf0acdf6b
Geraltwolf-bit <geraltwolf@yandex.ru>
2025-10-16 11:08:08 +0000

    fixed a bug

--- streamlit/demo.py
+++ streamlit/demo.py
@@ -11,6 +11,10 @@ list_of_means = []
 for i in range(1, 1001):
     list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
 
-fig, ax = plt.subplots()
-ax = plt.pie(list_of_means)
-st.pyplot(fig)
+fig1, ax1 = plt.subplots()
+ax1 = plt.hist(list_of_means)
+st.pyplot(fig1)
+
+fig2, ax2 = plt.subplots()
+ax2 = plt.hist(list_of_means)
+st.pyplot(fig2)
