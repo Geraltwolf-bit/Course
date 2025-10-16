be3366ba4d248a635301e5093c4256a2c2f4fbe1
Geraltwolf-bit <geraltwolf@yandex.ru>
2025-10-16 08:53:54 +0000

    fixed bug

--- streamlit/app.py
+++ streamlit/app.py
@@ -8,7 +8,7 @@ last_rows = np.random.randn(1, 1)
 chart = st.line_chart(last_rows)
 
 for i in range(1, 101):
-    new_rows = last_rows[-1, :] + np.random.randn(1, 5).cumsum(axis=1)
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=1)
     status_text.text("%i%% Complete" % i)
     chart.add_rows(new_rows)
     progress_bar.progress(i)
