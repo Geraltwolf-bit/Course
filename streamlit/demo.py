e251bf801017273ce71aff9083fe3a38f1aacde0
Geraltwolf-bit <geraltwolf@yandex.ru>
2025-10-16 10:38:40 +0000

    fixed a bug

--- streamlit/demo.py
+++ streamlit/demo.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 
 binom_dist = np.random.binomial(1, .5, 100)
+list_of_means = []
+
 st.write(np.mean(binom_dist))
+
+list_of_means = []
+for i in range(1, 1001):
+    list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
+
+fig, ax = plt.subplots()
+ax = plt.hist(list_of_means)
+st.pyplot(fig)
