5d7d9c7fdbec2186db47b1adcbbf84459fabc55d
Geraltwolf-bit <geraltwolf@yandex.ru>
2025-10-16 11:34:38 +0000

    fixed a bug

--- streamlit/demo.py
+++ streamlit/demo.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 
 perc_heads = st.number_input("Chance of coin landing on Heads", min_value=0.0, max_value=1.0, value = .5)
 binom_dist = np.random.binomial(1, perc_heads, 1000)
+app_title = st.text_input("Cool app!")
 list_of_means = []
 for i in range(0, 1000):
     list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
 
 fig, ax = plt.subplots()
 ax = plt.hist(list_of_means, range=[0, 1])
+plt.title(app_title)
 st.pyplot(fig)
