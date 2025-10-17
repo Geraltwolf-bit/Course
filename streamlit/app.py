2abe04fbd5145b523efeeead9f65dc3a96f1a851
Geraltwolf-bit <geraltwolf@yandex.ru>
2025-10-17 11:34:03 +0000

    fixed a bug

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,6 +3,6 @@ import numpy as np
 import pandas as pd
 
 st.title("Palmer's Penguins")
-path = '/workspaces/Course/streamlit/penguins.csv'
-penguins_df = pd.read_csv(path)
+url = 'https://github.com/tylerjrichards/Streamlit-for-Data-Science/blob/main/penguin_app/penguins.csv'
+penguins_df = pd.read_csv(url)
 st.write(penguins_df.head())
